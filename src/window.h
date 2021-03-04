92ada923248598ac8e2d3e51c086c45308f205bf
Russ Corbett-Detig <russ@Russs-MacBook-Air.local>
2021-03-04 09:34:40 -0800

    updated window size cutoff

--- src/window.h
+++ src/window.h
@@ -3,10 +3,9 @@
 
 void window( string id, vector<site> &germline, vector<site> &somatic, cmd_line &options ) {
  
-    for ( int s = options.window/2 ; s < 22e6 ; s += options.window ) {
-            
+    for ( int s = options.window/2 ; s <= germline.back().position - options.window/2 ; s += options.window ) { 
+           
         double somatic_k = golden_search_site( options, somatic, s ) ;
-        
         double germline_k = golden_search_site( options, germline, s ) ;
         
         cout << id << "\t" << s << "\t" << somatic_k << "\t" << germline_k << "\t" ;
